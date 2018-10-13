798ee3730b83bf637f7075e73231f468805b42b0
johnny-sack1 <jonymaka@gmail.com>
2018-10-13 11:35:50 +0200

    Implement the algorithm and test

--- src/main/java/com/codecool/tinkerTailor/TinkerTailor.java
+++ src/main/java/com/codecool/tinkerTailor/TinkerTailor.java
@@ -1,4 +1,50 @@
 package com.codecool.tinkerTailor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TinkerTailor {
+    private int n;
+    private int k;
+
+    public TinkerTailor(int n, int k) {
+        this.n = n;
+        this.k = k;
+    }
+
+    private List<Integer> createCircle() {
+        List<Integer> circle = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            circle.add(i);
+        }
+        return circle;
+    }
+
+    private int getPlayer(List<Integer> circle, int startingPlayerIndex) {
+        int current = 0;
+        for (int i = startingPlayerIndex; i < k + startingPlayerIndex; i++) {
+            if (i >= circle.size()) {
+                current = circle.get(i % circle.size());
+                System.out.println(current);
+            }
+            else {
+                current = circle.get(i);
+                System.out.println(current);
+            }
+        }
+        return current;
+    }
+
+    public List<Integer> start() {
+        List<Integer> circle = createCircle();
+        List<Integer> eliminatedPlayers = new ArrayList<>();
+        int startingPlayerIndex = 0;
+        for (int i = 0; i < n; i++) {
+            int eliminatedPlayer = getPlayer(circle, startingPlayerIndex);
+            startingPlayerIndex = circle.indexOf(eliminatedPlayer);
+            circle.remove(new Integer(eliminatedPlayer));
+            eliminatedPlayers.add(eliminatedPlayer);
+        }
+        return eliminatedPlayers;
+    }
 }
--- src/test/java/com/codecool/tinkerTailor/TinkerTailorTest.java
+++ src/test/java/com/codecool/tinkerTailor/TinkerTailorTest.java
@@ -1,4 +1,22 @@
 package com.codecool.tinkerTailor;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TinkerTailorTest {
+    @Test
+    void test() {
+        TinkerTailor game = new TinkerTailor(5, 3);
+        List<Integer> result = new ArrayList<>();
+        result.add(3);
+        result.add(1);
+        result.add(5);
+        result.add(2);
+        result.add(4);
+
+        assertEquals(result, game.start());
+    }
 }
